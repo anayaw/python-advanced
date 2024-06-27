4a2cbdd8f290b6a87ddbb39665def5cbe2e4cdb2
anaya :D <856938@pdsb.net>
2024-06-27 17:54:25 -0400

    Add files via upload

---
+++ classes n objects.py
@@ -0,0 +1,101 @@
+#1
+# class enimes():
+#     pass
+#2
+# class bosses(enimes):
+#     pass
+
+# class minibosses(enimes):
+#     pass
+
+# class bosscat(bosses):
+#     pass
+
+# class bossbat(bosses):
+#     pass
+
+# class rat(minibosses):
+#     pass
+#3
+# class weapons():
+#     pass
+# class shortrange(weapons):
+#     pass
+# class longrange(weapons):
+#     pass
+# class dagger(shortrange):
+#     pass
+# class slingshot(longrange):
+#     pass
+# class bowarrow(longrange):
+#     pass
+
+#4
+# class screenobjects():
+#     pass
+# class button(screenobjects):
+#     pass
+# class colorbuttons(button):
+#     pass
+# class clear(button):
+#     pass
+# class off(button):
+#     pass
+# class red(colorbuttons):
+#     pass
+# class green(colorbuttons):
+#     pass
+# class blue(colorbuttons):
+#     pass
+
+# class eraser(screenobjects):
+#     pass
+# class paintbrush(screenobjects):
+#     pass
+#5
+class food():
+    pass
+class pears(food):
+    pass
+class apples(food):
+    pass
+class fruits(food):
+    pass
+class bananas(food):
+    pass
+class fujiapples(food):
+    pass
+class grannysmith(food):
+    pass
+class mangoes(food):
+    pass
+class cauliflower(food):
+    pass
+class vegetables(food):
+    pass
+
+
+#6
+
+class books():
+    pass
+class selfhelpbooks(books):
+    pass
+class bookis (books):
+    pass
+class sciencefiction (books):
+    pass
+class horror (books):
+    pass
+class historical (books):
+    pass
+class fiction (books):
+    pass
+class howto (books):
+    pass
+class nonfiction (books):
+    pass
+class biographies (books):
+    pass
+class fantasy (books):
+    pass
