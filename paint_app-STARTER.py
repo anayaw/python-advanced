ef469970459de7862b161ac20c5d849903d7528a
anaya :3 <856938@pdsb.net>
2025-03-07 19:56:25 -0500

    paint_app-STARTER.py

--- paint_app-STARTER.py
+++ paint_app-STARTER.py
@@ -1,10 +1,10 @@
 '''
 Paint App Project
-Author:
-Date:
+Author: anaya
+Date: mar 7 , 2025
 
 [Project Description]
- 
+ im allowing you to paint take it or not !1!1!!!!!111!!1!!!!1!!!!1!
 '''
 
 from tkinter import *
@@ -17,29 +17,36 @@ TODO:
 
 '''
 root = Tk()
-root.title('Paint')
+root.title('paintt')
+
+c = Canvas(root,bg="white", height=1000, width=1000)
+f = Frame(root, bg="white",height=600,width=600)
+c.pack()
+f.pack()
+
 
 '''
 This project will use some global variables.
 COLOUR represents the current colour being used, and
 SIZE is the current size of the paintbrush.
 '''
-COLOUR = 'black'
+COLOR = 'black'
 SIZE = 10
+colors= ["red", "orange", "yellow", "green", "blue", "purple", "pink"]
 
-class ColourButton():
+class ColorButton():
      '''
      A ColourButton is the class for the buttons at the
      top of the screen that change the brush colour.
      '''
-     def __init__(self, parent, colour):
+     def __init__(self, color):
           '''
           This is the function called when creating a ColourButton.
-          TODO:
+          TODO:i done yayayay 244 !1!1!1!!
           - fill in the line indicated below.
           '''
-          self.colour = colour
-          self.Button = '''#FILL IN'''
+          self.color = color
+          self.Button = Button(f, bg=color, command=self.update) 
      
      def update(self):
           '''
@@ -48,9 +55,10 @@ class ColourButton():
           When a ColourButton is pressed, it must ONLY change the global COLOUR
           variable to be this ColourButton's colour attribute/
           TODO:
-          - fill in this function. It should be 2 lines.
+          - fill in this function. It should be 2 lines. i done
           '''
-          pass
+          global COLOR
+          COLOR = self.color
      
 def draw_circle(event):
     '''
@@ -60,6 +68,7 @@ def draw_circle(event):
     TODO:
     - finish this function (should be 1-2 lines)
     '''
+    c.create_oval(event.x, event.y, event.x + SIZE.get(), event.y + SIZE.get(), fill= COLOR, outline= COLOR)
     
 def clear_canvas():
     '''
@@ -67,7 +76,9 @@ def clear_canvas():
     It must clear all drawings from the canvas.
     TODO:
     - finish this function (1 line)
+    
     '''
+    c.delete('all')
 
 #################### MAIN CODE #########################
 
@@ -80,9 +91,9 @@ TODO:
 - add at least 4 more colours to the colours list
 '''
 
-colours = ['red']
-for i in range(len(colours)):
-    x = ColourButton(colours[i])
+colors= ["red", "orange", "yellow", "green", "blue", "purple", "pink"]
+for i in range(len(colors)):
+    x = ColorButton(colors[i])
     x.Button.grid(row=0, column=i)
 
 '''
@@ -91,12 +102,12 @@ Next create the CLEAR button and the width slider.
 TODO:
 - complete the code fragments below
 '''
-clear_button = '''# FILL IN'''
+clear_button = Button(f, text='clear', command= clear_canvas)
 clear_button.grid(row = 0, column = i + 1)
 
 SIZE = IntVar()
 '''# We are using the global variable as the int var so it is always updated'''
-scalebar = '''# Fill in - there are many ways to do this'''
+scalebar = Scale(f, variable = SIZE, bg = 'grey', tickinterval=1,  resolution = 1, orient = HORIZONTAL, from_ = 1, to = 10)
 scalebar.grid(row = 0, column = i + 2)
 
 '''
@@ -105,3 +116,6 @@ TODO:
 - bind the letter q to quitting the program
 - mainloop!
 '''
+c.bind("<B1-Motion>", draw_circle) 
+root.bind("<q>", quit)
+root.mainloop()
